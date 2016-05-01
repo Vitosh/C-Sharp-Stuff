91bd0545aaf04e8aec8848fc9a4c808bfa56881b
Vitosh Academy <vitosh.doynov@gmail.com>
2016-05-01 21:42:45 +0200

    Added files via upload

---
+++ linq_test/Company.cs
@@ -0,0 +1,52 @@
+﻿namespace linq_test
+{
+    class Company
+    {
+        private string _companyName;
+        private string _companyCEO;
+        private int _companyProfit;
+        private string _companyCountry;
+
+        public Company(string name, string ceo = "No CEO", int profit = 0, string country = "Bulgaria")
+        {
+            _companyName = name;
+            _companyCEO = ceo;
+            _companyProfit = profit;
+            _companyCountry = country;
+        }
+
+        public string name
+        {
+            get { return _companyName; }
+        }
+
+        public int profit
+        {
+            get {return _companyProfit;}
+        }
+
+        public string country 
+        {
+            get {return _companyCountry;}
+        }
+
+        public string ceo
+        {
+            get
+            {
+                return _companyCEO;
+            }
+            set
+            {
+                _companyCEO = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("This company is named {0}. The CEO is {1}. The profit is {2}. It is located in {3}.",
+                                this.name, this.ceo, this.profit, this.country);
+        }
+
+    }
+}
---
+++ linq_test/LinqApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_test
+{
+    class LinqApp
+    {
+        static void Main()
+        {
+            Company c1 = new Company("VitoshAcademy", "Vitosh", 10, "Bulgaria");
+            Company c2 = new Company("HereComesTheCode", "Vitosh", 12, "Russia");
+            Company c3 = new Company("SomeBadCompany", "Vitosh", 14, "Germany");
+            Company c4 = new Company("SomeBadCompany2", "Creepy", 16, "Germany");
+            Company c5 = new Company("SomeBadCompany3", "Creepy");
+            Company c6 = new Company("SomeBadCompany4", "Vitosh");
+
+            List<Company> companies = new List<Company> { c1, c2, c3, c4, c5, c6 };
+
+            var bulgarian_companies = from my_company in companies
+                                      where my_company.country == "Bulgaria"
+                                      select my_company;
+            Console.WriteLine("Bulgarian companies only:");
+            foreach (var company in bulgarian_companies)
+            {
+                Console.WriteLine(company.ToString());
+            }
+
+            Console.WriteLine("\nCompanies with a positive profit :");
+            var companies_with_profit = from my_company in companies
+                                        where my_company.profit > 0
+                                        select my_company;
+
+            foreach (var company in companies_with_profit)
+            {
+                Console.WriteLine(company.ToString());
+            }
+        }
+    }
+}
