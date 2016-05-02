e2f5fd52a689244cbb082452146841a04d4611bd
Vitosh Academy <vitosh.doynov@gmail.com>
2016-05-02 23:40:34 +0200

    Added files via upload

---
+++ Fibonacci.Tests/FibonacciTests.cs
@@ -0,0 +1,62 @@
+﻿namespace Fibonacci
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void TestWith10()
+        {
+            long l_fib = 10;
+            FibonacciChecks.my_memo = new long[l_fib + 1];
+
+            long l_result = FibonacciChecks.FibWithMemo(l_fib);
+            Assert.AreEqual(l_result, 55);
+        }
+
+        [TestMethod]
+        public void TestWith20()
+        {
+            long l_fib = 20;
+            FibonacciChecks.my_memo = new long[l_fib + 1];
+
+            long l_result = FibonacciChecks.FibWithMemo(l_fib);
+            Assert.AreEqual(l_result, 6765);
+        }
+
+        [TestMethod]
+        public void TestWith0()
+        {
+            long l_fib = 0;
+            FibonacciChecks.my_memo = new long[l_fib + 1];
+
+            long l_result = FibonacciChecks.FibWithMemo(l_fib);
+            Assert.AreEqual(l_result, 0);
+        }
+
+        [TestMethod]
+        public void TestWith1()
+        {
+            long l_fib = 1;
+            FibonacciChecks.my_memo = new long[l_fib + 1];
+
+            long l_result = FibonacciChecks.FibWithMemo(l_fib);
+            Assert.AreEqual(l_result, 1);
+        }
+
+
+        [TestMethod]
+        public void TestWith2()
+        {
+            long l_fib = 2;
+            FibonacciChecks.my_memo = new long[l_fib + 1];
+
+            long l_result = FibonacciChecks.FibWithMemo(l_fib);
+            Assert.AreEqual(l_result, 1);
+        }
+
+    }
+}
---
+++ Fibonacci/FibonacciChecks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Fibonacci
+{
+    public class FibonacciChecks
+    {
+        public static long[] my_memo;
+        public static DateTime my_time;
+        public static long my_counter;
+
+        public static void Main()
+        {
+            Console.WriteLine("Enter number between 2 and 1300 for Fibonacci calculation:");
+            long k = long.Parse(Console.ReadLine());
+            my_memo = new long[k + 1];
+
+            my_counter = 0;
+            my_time = DateTime.Now;
+
+            Console.WriteLine("\n{0}", FibWithMemo(k));
+            Console.WriteLine("Calculations {0}", my_counter);
+            Console.WriteLine(DateTime.Now - my_time);
+        }
+
+        public static long FibWithMemo(long counter_inside, bool with_memo = true)
+        {
+            if (with_memo)
+            {
+                if (my_memo[counter_inside] != 0) return my_memo[counter_inside];
+            }
+
+            my_counter++;
+
+            if (counter_inside == 0) return 0;
+            if (counter_inside == 1) return 1;
+            my_memo[counter_inside] = FibWithMemo(counter_inside - 1, with_memo) + FibWithMemo(counter_inside - 2, with_memo);
+            return my_memo[counter_inside];
+        }
+    }
+}
