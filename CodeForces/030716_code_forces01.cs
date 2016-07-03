1d9c21a2fbb45f407196ea94abd94b3fc0c8df2f
Vitosh Academy <vitosh.doynov@gmail.com>
2016-07-03 20:22:31 +0200

    Add files via upload

---
+++ CodeForces/030716_code_forces01.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class test_code_forces01
+{
+    static void Main()
+    {
+
+        string[] input = Console.ReadLine().Split();
+        int enemies_count = int.Parse(input[0]);
+        int days_count = int.Parse(input[1]);
+        List<int> results = new List<int>();
+
+        string number_max = CheckNumber(enemies_count);
+        string current_string = "";
+        int biggest_sequence = 0;
+
+        for (int i = 0; i < days_count; i++)
+        {
+            current_string = Console.ReadLine();
+            if (current_string.Equals(number_max,StringComparison.Ordinal))
+            {
+                biggest_sequence = 0; 
+            }
+            else
+            {
+                biggest_sequence++;
+            }
+            results.Add(biggest_sequence);
+        }
+        Console.WriteLine(results.Max());
+    }
+
+    static string CheckNumber(int enemies_count)
+    {
+        string result = "";
+        for (int i = 0; i < enemies_count; i++)
+        {
+            result += "1";
+        }
+        return result;
+    }
+}
---
+++ CodeForces/030716_code_forces02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class test_code_forces02
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(input+Reverse(input));
+    }
+
+    public static string Reverse(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+}
+
