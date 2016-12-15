228ad775530427f693928e6276fcb07b1a8e23d1
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-12-15 14:11:43 +0100

    Add files via upload

---
+++ IntSense/Demo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IntDemo
+{
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Demo : IIntellisenseDemo
+    {
+        public int numberBy2;
+        public int numberBy3;
+
+        public int NumberBy3
+        {
+            get
+            {
+                return numberBy3;
+            }
+            set
+            {
+                numberBy3 = value * 3;
+            }
+
+        }
+
+        public int NumberBy2
+        {
+            get
+            {
+                return numberBy2;
+            }
+            set
+            {
+                numberBy2 = value * 2;
+            }
+        }
+        public string TestString(string name)
+        {
+            return "Here comes " + name;
+        }
+    }
+}
---
+++ IntSense/IIntellisenseDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IntDemo
+{
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+
+    public interface IIntellisenseDemo
+    {
+        int NumberBy3 { get; set; }
+        int NumberBy2 { get; set; }
+        string TestString(string name);
+    }
+}
