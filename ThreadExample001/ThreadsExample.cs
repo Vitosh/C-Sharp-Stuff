4c4fa0f8b1f42caa28fe72887789a41427533f20
Vitosh Academy <Vitosh@users.noreply.github.com>
2017-01-09 00:02:21 +0100

    Add files via upload

---
+++ ThreadExample001/ThreadsExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace threads
+{
+    class ThreadsExample
+    {
+        static void Main()
+        {
+            Writer oWriter1 = new Writer();
+            Writer oWriter2 = new Writer();
+            Writer oWriter3 = new Writer();
+
+            Thread oThread1 = new Thread(() => oWriter1.WriteSomething("I am the first"));
+            Thread oThread2 = new Thread(() => oWriter2.WriteSomething("2222"));
+            Thread oThread3 = new Thread(() => oWriter3.WriteSomethingOnce());
+
+            oThread1.Start();
+            oThread2.Start();
+            oThread3.Start();
+
+            Thread.Sleep(100);
+
+            oThread1.Abort();
+            oThread2.Abort();
+            oThread3.Abort();
+        }
+    }
+}
---
+++ ThreadExample001/Writer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threads
+{
+    public class Writer
+    {
+        public void WriteSomething(string strSomething)
+        {
+            while (true)
+            {
+                Console.WriteLine(strSomething);
+            }
+        }
+
+        public void WriteSomethingOnce()
+        {
+            Console.WriteLine("Writing just once");
+        }
+
+    }
+}
