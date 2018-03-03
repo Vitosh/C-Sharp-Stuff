494bb63fe40db5e3312ed5dbd3d03933f889ebbc
Vitosh Academy <Vitosh@users.noreply.github.com>
2018-03-03 22:38:55 +0100

    Add files via upload

---
+++ WindowFormExample/NewWindowForm/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace NewWindowForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(13, 13);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(179, 20);
+            this.tbInput.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(198, 13);
+            this.lblResult.MaximumSize = new System.Drawing.Size(180, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(47, 13);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "lblResult";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(430, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoEllipsis = true;
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(10, 96);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(35, 13);
+            this.lblPath.TabIndex = 4;
+            this.lblPath.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.ClientSize = new System.Drawing.Size(517, 189);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "VitoshAcademy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblPath;
+    }
+}
+
---
+++ WindowFormExample/NewWindowForm/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace NewWindowForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblResult.Text = "Please, select a file.";
+            lblPath.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application excel = null;
+            excel = new Excel.Application();
+            excel.Visible = true;
+            Excel.Workbook wkb = null;
+
+            wkb = Open(excel, lblPath.Text);            
+            Excel.Range searchedRange = excel.get_Range("A1", "XFD1048576");
+            Excel.Range currentFind = searchedRange.Find(tbInput.Text);
+            string displayResult = "";
+
+            if (currentFind != null)
+            {
+                displayResult = "Found at \ncolumn - " + currentFind.Column + 
+                                            "\nrow - " + currentFind.Row;
+            }
+            else
+            {
+                displayResult = "The searched string \"" + tbInput.Text +
+                        "\" is not found.";
+            }
+            lblResult.Text = displayResult;
+            wkb.Close(true);
+            excel.Quit();
+        }
+
+        public static Excel.Workbook Open(Excel.Application excelInstance,
+                        string fileName, bool readOnly = false, bool editable = true,
+                        bool updateLinks = true)
+        {
+            Excel.Workbook book = excelInstance.Workbooks.Open(
+                fileName, updateLinks, readOnly,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, editable, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing);
+            return book;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = true;
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                lblPath.Text = choofdlog.FileName;
+                lblResult.Text = "File is selected. Now enter a word and press the \"GO\" button.";      
+            }
+        }
+    }
+}
