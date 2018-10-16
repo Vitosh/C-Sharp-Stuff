9ac83afe805e8d1b5e5b1bb25293b1f9ce851e5b
Vitosh Academy <Vitosh@users.noreply.github.com>
2018-10-16 22:03:14 +0200

    Update FatSeparatorSplit.cs

--- FatSeparatorSplit.cs
+++ FatSeparatorSplit.cs
@@ -1,3 +1,14 @@
-        string test = "AAAaaFATSEPARATORzzZZZ";
-        string first = test.Substring(0, test.IndexOf("FATSEPARATOR"));
-        string second = test.Substring(test.IndexOf("FATSEPARATOR") + 12);
+using System;
+
+class StartUp
+{
+    static void Main()
+    {
+        string sample = "*AAAaa*SEPARATOR*zzZZZ*";
+        string first = sample.Substring(0, sample.IndexOf("SEPARATOR"));
+        string second = sample.Substring(sample.IndexOf("SEPARATOR") + "SEPARATOR".Length);
+
+        Console.WriteLine(first);
+        Console.WriteLine(second);
+    }
+}
