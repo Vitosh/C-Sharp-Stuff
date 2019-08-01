e30bc45a801674b19e8ddb9e4ba3268e89e5ace3
Vitosh Academy <Vitosh@users.noreply.github.com>
2019-08-01 14:37:41 +0200

    Add files via upload

---
+++ FibonacciWithXUnit/Fibonacci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciWithXUnit
+{
+    public class Fibonacci
+    {
+        public long Calculate(int position)
+        {
+            //if (position == 1) return 1;
+            //if (position == 2) return 1;
+            //return Calculate(position - 1) + Calculate(position - 2);
+
+            if (position == 1 || position == 2)
+            {
+                return 1;
+            }
+
+            int fib1 = 1;
+            int fib2 = 1;
+            int result = -1;
+
+            for (int i = 3; i <= position; i++)
+            {
+                result = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = result;
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ FibonacciWithXUnit/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FibonacciWithXUnit
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Fibonacci fibonacci = new Fibonacci();
+            for (int i = 1; i < 30; i++)
+            {
+                Console.WriteLine(fibonacci.Calculate(i));
+            }
+        }
+    }
+}
