db7c065c2a6b3ffa473d7c6caa16907d61ce2ad5
Vitosh Academy <Vitosh@users.noreply.github.com>
2019-12-14 20:24:27 +0100

    Add files via upload

---
+++ Build a .Net library/ShowCase/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UtilityLibraries;
+
+class Startup
+{
+    static void Main()
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+            {
+                ResetConsole();
+            }               
+
+            string input = Console.ReadLine();
+            if (String.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input} {"Begins with uppercase? ",30}: " +
+                              $"{(input.StartsWithUpper() ? "Yes" : "No")}\n");
+            row += 3;
+        } while (true);
+        return;
+
+
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine("\nPress <Enter> only to exit; otherwise, enter a string and press <Enter>:\n");
+            row = 3;
+        }
+    }
+}
---
+++ Build a .Net library/StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilityLibraries;
+
+namespace StringLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStartsWithUpper()
+        {
+            string[] words = { "Alphabet", "Zulu", "ABC", "Витош", "ВитошАкадемия.ком" };
+            foreach (string word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsTrue(result, String.Format("Expected for '{0}': true; Actual {1}",word, result));
+            }
+        }
+
+        [TestMethod]
+        public void TestDoesNotStartWithUpper()
+        {
+            string[] words = { "alphabet", "zulu", "aBC", "ш", "ком","55",".","" };
+            foreach (string word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsFalse(result, String.Format("Expected for '{0}': true; Actual {1}", word, result));
+            }
+        }
+
+        [TestMethod]
+        public void TestWithNullOrEmpty()
+        {
+            string[] words = {string.Empty, null};
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsFalse(result, String.Format("Expected for '{0}': false; Actual {1}",
+                    word == null ? "<null>" : word, result ));
+            }
+        }
+    }
+}
